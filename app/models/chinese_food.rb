c7e47238fd5e58b1d3dc74349355e647436848d8
eri <k.erii0341115@gmail.com>
2024-06-10 17:20:48 +0900

    update

--- app/models/chinese_food.rb
+++ app/models/chinese_food.rb
@@ -1,3 +1,3 @@
-class ChineseFood < ActiveRecord::Base
-  belongs_to :food
+class ChineseFood < Food
+
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,4 @@ class Food < ApplicationRecord
   has_many :order_foods
   has_many :orders, through: :order_foods
   belongs_to :shop
-  has_many :japanese_food
-  has_many :chinese_food
-  has_many :italian_food
 end
